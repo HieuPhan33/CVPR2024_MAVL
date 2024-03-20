917f476785708c1c5f25a8e03e26c8989bb6c5e9
HieuPhan33 <hieup007@gmail.com>
2024-03-20 11:31:22 +1100

    simplify model

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 cd Pretrain/data_file
 gdown --folder 1EN-oHVk513qmoehWypkrJ2Ero5PoUmId
 cd ../../Pretrain/models
-wget https://github.com/openai/CLIP/raw/main/clip/bpe_simple_vocab_16e6.txt.gz
+# wget https://github.com/openai/CLIP/raw/main/clip/bpe_simple_vocab_16e6.txt.gz
